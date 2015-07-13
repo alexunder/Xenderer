d68d8e40642c8001a1a8111e9d40925b4ef20eeb
Alexunder <xalexu@gmail.com>
2015-07-13 18:55:23 +0800

    continue implementing

--- XRayTracer/XImage.cpp
+++ XRayTracer/XImage.cpp
@@ -28,3 +28,52 @@ XImage::XImage(int width, iny height)
 		mRaster[i] = new XRGB[mHeight];
 }
 
+XImage::XImage(int width, iny height, XRGB background)
+{
+	mWidth = width;
+	mHeight = height;
+
+	//allocate memory for raster
+	mRaster = new XRGB*[mWidth];
+
+	int i;
+	int j;
+
+	for (i = 0; i < mWidth; i++)
+	{
+		mRaster[i] = new XRGB[mHeight];
+		//Assign 'background' to each element.
+		for (j = 0; i < mHeight; i++)
+			mRaster[i][j] = background;;
+	}
+}
+
+bool XImage::setPixel(int x, int y, const XRGB & color)
+{
+	//Check for out of boulds error
+	if ( x < 0 || x > mWidth)
+		return false;
+	if ( y < 0 || y > mHeight)
+		return false;
+
+	mRaster[x][y] = color; 
+}
+
+void XImage::gammaCorrect(float gamma)
+{
+	XRGB temp;
+	float power = 1.0 / gama;
+
+	int i;
+	int j;
+
+	for ( i = 0; i < mWidth; i++)
+	for ( j = 0; j < mHeight; j++)
+	{
+		temp = mRaster[i][j];
+		mRaster[i][j] = XRGB(pow(temp.r(), power),
+							 pow(temp.g(), power),
+							 pow(temp.b(), power),
+							 );
+	}
+}
