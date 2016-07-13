db66b0c5590756299e96dccc56b8fd07dd1c642f
Alexander Hsu <alexanderxu@asrmicro.com>
2016-07-13 15:48:40 +0800

    Add the new testcase

--- SmartRasterizer/linux/main.cpp
+++ SmartRasterizer/linux/main.cpp
@@ -9,10 +9,13 @@
 #include "RendererCanvas.h"
 #include "ModelLoader.h"
 
+//#define TEST_DDA_LINE
+
 char * input_file = NULL;
 int    width = 800;
 int    height = 800;
-bool testmode = false;
+bool test_line_mode = false;
+bool test_triangle_mode = false;
 RendererCanvas gCanvas;
 
 gboolean on_expose_event (GtkWidget * widget, GdkEventExpose *event, 
@@ -52,9 +55,60 @@ void parseArgs(int argc, char **argv)
         }
         else if (!strcmp(argv[i], "-testline"))
         {
-            testmode = true;
+            test_line_mode = true;
+        }
+        else if (!strcmp(argv[i], "-triangle"))
+        {
+            test_triangle_mode = true;
         }
     }
+}
+
+void test_draw_lines()
+{
+    //Draw x and y Axis
+    Color colorAxis(0.0, 0.0, 0.0);
+    Color colorLine(1.0, 0.0, 1.0);
+    gCanvas.DrawLineWithBresenham(colorAxis, 0, height/2, width - 1, height/2);
+    gCanvas.DrawLineWithBresenham(colorAxis, width/2, 0, width/2, height - 1);
+
+    //Then draw 8 cases
+#ifdef TEST_DDA_LINE
+    gCanvas.DrawLineWithDDA(colorLine, width/2, height/2, width/2 + 200, height/2 + 50);
+    gCanvas.DrawLineWithDDA(colorLine, width/2, height/2, width/2 + 50, height/2 + 200);
+    gCanvas.DrawLineWithDDA(colorLine, width/2, height/2, width/2 - 200, height/2 - 50);
+    gCanvas.DrawLineWithDDA(colorLine, width/2, height/2, width/2 - 50, height/2 - 200);
+
+    gCanvas.DrawLineWithDDA(colorLine, width/2, height/2, width/2 + 200, height/2 - 50);
+    gCanvas.DrawLineWithDDA(colorLine, width/2, height/2, width/2 + 50, height/2 - 200);
+    gCanvas.DrawLineWithDDA(colorLine, width/2, height/2, width/2 - 200, height/2 + 50);
+    gCanvas.DrawLineWithDDA(colorLine, width/2, height/2, width/2 - 50, height/2 + 200);
+#else
+    gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 + 200, height/2 + 50);
+    gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 + 50, height/2 + 200);
+    gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 - 200, height/2 - 50);
+    gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 - 50, height/2 - 200);
+
+    gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 + 200, height/2 - 50);
+    gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 + 50, height/2 - 200);
+    gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 - 200, height/2 + 50);
+    gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 - 50, height/2 + 200);
+#endif
+}
+
+void test_triangle_lines()
+{
+    Color color(0.0, 0.0, 1.0);
+    int x0 = 10;
+    int y0 = 10;
+    int x1 = 600;
+    int y1 = 156;
+    int x2 = 650;
+    int y2 = 420;
+
+    gCanvas.DrawLineWithBresenham(color, x0, y0, x1, y1);
+    gCanvas.DrawLineWithBresenham(color, x1, y1, x2, y2);
+    gCanvas.DrawLineWithBresenham(color, x2, y2, x0, y0);
 }
 
 int main (int argc, char *argv[])
@@ -74,43 +128,29 @@ int main (int argc, char *argv[])
        }
     }
 
-    if (testmode == true)
+    gtk_init (&argc, &argv);
+    gCanvas.CreateFrameBuffer(width, height);
+
+    if (test_line_mode == true || test_triangle_mode == true)
 	{
-        gtk_init (&argc, &argv);
 
-        gCanvas.CreateFrameBuffer(width, height);
-        //Color color1(0.0,0.0,1.0);
-        //gCanvas.DrawLineWithDDA(color1, 2, 2, 200, 500);
-        //Draw x and y Axis
-        Color colorAxis(0.0, 0.0, 0.0);
-        Color colorLine(1.0, 0.0, 1.0);
-        gCanvas.DrawLineWithBresenham(colorAxis, 0, height/2, width - 1, height/2);
-        //gCanvas.DrawLineWithDDA(colorAxis, 0, height/2, width - 1, height/2);
-        gCanvas.DrawLineWithBresenham(colorAxis, width/2, 0, width/2, height - 1);
-        //gCanvas.DrawLineWithDDA(colorAxis, width/2, 0, width/2, height - 1);
+        if (test_line_mode == true)
+            test_draw_lines();
 
-        //Then draw 8 cases
-        gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 + 200, height/2 + 50);
-        gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 + 50, height/2 + 200);
-        gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 - 200, height/2 - 50);
-        gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 - 50, height/2 - 200);
-
-        gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 + 200, height/2 - 50);
-        gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 + 50, height/2 - 200);
-        gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 - 200, height/2 + 50);
-        gCanvas.DrawLineWithBresenham(colorLine, width/2, height/2, width/2 - 50, height/2 + 200);
-
-        GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-        g_signal_connect(window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
-        gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-        gtk_widget_set_size_request(window, width, height);
-        gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
-
-        g_signal_connect(window, "expose-event", G_CALLBACK(on_expose_event), NULL);
-        gtk_widget_set_app_paintable(window, TRUE);
-        gtk_widget_show_all(window);
-        gtk_main();
+        if (test_triangle_mode == true)
+            test_triangle_lines();
     }
+
+    GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    g_signal_connect(window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_widget_set_size_request(window, width, height);
+    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+
+    g_signal_connect(window, "expose-event", G_CALLBACK(on_expose_event), NULL);
+    gtk_widget_set_app_paintable(window, TRUE);
+    gtk_widget_show_all(window);
+    gtk_main();
 
     if (pMesh != NULL)
         delete pMesh;
