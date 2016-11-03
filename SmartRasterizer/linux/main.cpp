8abafa0144548c4b83353de797ec460f66b2f2e3
Alexander Hsu <alexanderxu@asrmicro.com>
2016-11-03 18:10:09 +0800

    Make the project platform independent.

--- SmartRasterizer/linux/main.cpp
+++ SmartRasterizer/linux/main.cpp
@@ -3,8 +3,7 @@
  * All rights reserved
  */
 
-#include <cairo.h>
-#include <gtk/gtk.h>
+#include "os_layer.h"
 #include "stdafx.h"
 #include "RendererCanvas.h"
 #include "ModelLoader.h"
@@ -18,34 +17,6 @@ bool test_line_mode = false;
 bool test_triangle_mode = false;
 bool test_rasterize_triangle_mode = false;
 RendererCanvas gCanvas;
-
-gboolean on_expose_event (GtkWidget * widget, GdkEventExpose *event, 
-                          gpointer data) {
-	cairo_t *cr = gdk_cairo_create(widget->window);
-	GdkPixbuf * pixframebuffer = gdk_pixbuf_new_from_data(
-                        (const unsigned char*)gCanvas.getframeBuffer(), 
-						GDK_COLORSPACE_RGB, TRUE, 8, width, height,
-						width*4, NULL, NULL);
-	gdk_cairo_set_source_pixbuf(cr, pixframebuffer, 0, 0);
-	cairo_paint(cr);
-	g_object_unref(pixframebuffer);
-
-	cairo_destroy(cr);
-	return FALSE;
-}
-
-void UiPrepare() {
-    GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    g_signal_connect(window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-    gtk_widget_set_size_request(window, width, height);
-    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
-
-    g_signal_connect(window, "expose-event", G_CALLBACK(on_expose_event), NULL);
-    gtk_widget_set_app_paintable(window, TRUE);
-    gtk_widget_show_all(window);
-    gtk_main();
-}
 
 void parseArgs(int argc, char **argv) {
     for (int i = 1; i < argc; i++) {
@@ -70,8 +41,7 @@ void parseArgs(int argc, char **argv) {
     }
 }
 
-void test_draw_lines()
-{
+void test_draw_lines() {
     //Draw x and y Axis
     Color colorAxis(0.0, 0.0, 0.0);
     Color colorLine(1.0, 0.0, 1.0);
@@ -102,8 +72,7 @@ void test_draw_lines()
 #endif
 }
 
-void test_triangle_lines()
-{
+void test_triangle_lines() {
     Color color(0.0, 0.0, 1.0);
     int x0 = 10;
     int y0 = 10;
@@ -117,8 +86,7 @@ void test_triangle_lines()
     gCanvas.DrawLineWithBresenham(color, x2, y2, x0, y0);
 }
 
-void test_triangle_rasterize()
-{
+void test_triangle_rasterize() {
     Color color(0.0, 0.0, 1.0);
     int x0 = 10;
     int y0 = 10;
@@ -130,29 +98,27 @@ void test_triangle_rasterize()
 }
 
 
-int main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]) {
     parseArgs(argc, argv);
 
     MeshObjectModel * pMesh = NULL;
 
-    if (input_file != NULL)
-    {
+    if (input_file != NULL) {
        bool ret = loadObjModel(input_file, &pMesh);
 
-       if (ret == false)
-       {
+       if (ret == false) {
             fprintf(stderr, "Loaded OBJ file failed, just Exit.\n");
             return 1;
        }
     }
 
-    gtk_init (&argc, &argv);
     gCanvas.CreateFrameBuffer(width, height);
 
+    UIContext * pContext = new UIContext(width, height, &gCanvas);
+    pContext->Init(argc, argv);
+
     if (test_line_mode == true || test_triangle_mode == true
-                               || test_rasterize_triangle_mode == true )
-	{
+                               || test_rasterize_triangle_mode == true ) {
 
         if (test_line_mode == true)
             test_draw_lines();
@@ -164,10 +130,11 @@ int main (int argc, char *argv[])
             test_triangle_rasterize();
     }
 
-    UiPrepare();
+    pContext->Prepare();
 
     if (pMesh != NULL)
         delete pMesh;
 
+    delete pContext;
     return 0;
 }
